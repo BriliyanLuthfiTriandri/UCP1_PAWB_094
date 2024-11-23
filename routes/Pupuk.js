5910ebb17a5b82b50aff518bf7be9a7cc9e57c60
BriliyanluthfiTriandri <briliyanlt@gmail.com>
2024-11-23 20:26:25 +0700

    menabahkan data awal untuk pupuk

--- routes/Pupuk.js
+++ routes/Pupuk.js
@@ -1,2 +1,17 @@
 import express, { Router } from "express";
 const router = express.Router();
+
+const pupuk = [
+    {
+        id: 1,
+        namapupuk: "pupuk enak",
+        jenis: "organik",
+        kadaluarsa: 2021,
+    },
+    {
+        id: 2,
+        namapupuk: "sehat bergizi",
+        jenis: "sintetis",
+        kadaluarsa: 2024,
+    },
+];
