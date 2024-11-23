d05325b64a94e5d8a53a67891315c94e36ee3b96
BriliyanluthfiTriandri <briliyanlt@gmail.com>
2024-11-23 20:40:32 +0700

    menambahkan route GET untuk endpoint root dan menambahkan pesan

--- index.js
+++ index.js
@@ -11,3 +11,9 @@ app.use(bodyParser.json());
 app.use("/bibit", bibitRoute);
 app.use("/pupuk", pupukRoute);
 
+
+app.get("/", (req, res) => {
+    console.log(["GET ROUTE"]);
+    res.send("Selamat Pagi");
+});
+
