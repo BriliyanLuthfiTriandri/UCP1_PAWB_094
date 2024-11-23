40ecce730d08eaf8d303ccf8334dbd8ebce13f9a
BriliyanluthfiTriandri <briliyanlt@gmail.com>
2024-11-23 20:28:53 +0700

    menambahkan endpoint POST untuk menambahkan data pupuk baru

--- routes/Pupuk.js
+++ routes/Pupuk.js
@@ -19,3 +19,14 @@ router.get("/", (req, res) => {
     res.send(pupuk);
 });
 
+router.post('/', (req, res) => {
+    const newPupuk = {
+        id: pupuk.length + 1,
+        namapupuk: req.body.namapupuk,
+        jenis: req.body.jenis,
+        kadaluarsa: req.body.kadaluarsa
+    };
+    pupuk.push(newPupuk);
+    res.status(201).json(newPupuk);
+});
+
