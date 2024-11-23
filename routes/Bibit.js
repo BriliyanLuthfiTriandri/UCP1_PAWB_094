e252ffc682ec3c74e6ff4203bdb4b1cecf08767c
BriliyanluthfiTriandri <briliyanlt@gmail.com>
2024-11-23 20:08:46 +0700

    menambahkan variabel dan mengisi data awal untuk bibit

--- routes/Bibit.js
+++ routes/Bibit.js
@@ -1,2 +1,16 @@
 import express, { Router } from "express";
 const router = express.Router();
+const bibit = [
+    {
+        id: 1,
+        namabibit: "kangkung",
+        jenis: "sayuran",
+        kadaluarsa: 2012,
+    },
+    {
+        id: 2,
+        namabibit: "bayam",
+        jenis: "sayuran",
+        kadaluarsa: 2022,
+    },
+];
