24011232e51003a5335b0d4594180c582d3c0d3c
BriliyanluthfiTriandri <briliyanlt@gmail.com>
2024-11-23 20:36:39 +0700

    menyiapkan server express dengan body-parser dan import route untuk bibit dan pupuk

--- index.js
+++ index.js
@@ -1,20 +1,5 @@
 import express from "express";
 import bodyParser from "body-parser";
-import mobilRoute from "./routes/Mobil.js";
-import mahasiswaRoute from "./routes/Mahasiswa.js";
+import bibitRoute from "./routes/Bibit.js";
+import pupukRoute from "./routes/Pupuk.js";
 
-const app = express();
-const PORT = 8000;
-
-app.use("/bibit", mobilRoute);
-app.use("/pupuk", mahasiswaRoute);
-
-
-app.get("/", (req, res) => {
-    console.log(["GET ROUTE"]);
-    res.send("Selamat Pagi");
-});
-
-app.use(bodyParser.json());
-app.listen(PORT, () =>
-    console.log(`Server berjalan di port : http://localhost:${PORT}`));
